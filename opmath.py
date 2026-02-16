02e93d2da407e8464f9279c2865e61684edd1eea
Pivetzen <pivetzen@gmail.com>
2026-02-16 10:27:24 -0300

    Inclusão de operações matematicas

--- opmath.py
+++ opmath.py
@@ -1,1 +1,16 @@
 
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+operacao = input("Digite a operaçãoque deseja realizar (+, -, *, /): ")
+
+if operacao == '+':
+    print(num1 + num2)
+elif operacao == '-':
+    print(num1 - num2)
+elif operacao == '*':
+    print(num1 * num2)
+elif operacao == '/':
+    print(num1 / num2)
+else:
+    print("Operação Inválida")
